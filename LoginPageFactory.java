3d4473614e856891ded2d49439a66b0580d926cd
srjsingh <45200521+srjsingh@users.noreply.github.com>
2018-11-29 11:39:02 +0530

    Add files via upload

--- LoginPageFactory.java
+++ LoginPageFactory.java
@@ -1,57 +1,112 @@
-package pomexamples.pages;
+package application.login.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.Select;
 
 public class LoginPageFactory {
-	WebDriver driver;
-	@FindBy(id="txtUserName")
-	@CacheLookup
+ WebDriver driver;
+ @FindBy(xpath="html/body/div[2]/table/tbody/tr/th/a")
+ @CacheLookup
+	WebElement admin_login;
+ 
+ @FindBy(how=How.NAME, using="uname")
+ @CacheLookup
 	WebElement uname;
-	
-	@FindBy(how=How.NAME, using="txtFN")
-	@CacheLookup
-	WebElement fname;
-	
-	@FindBy(how=How.NAME, using="txtLN")
-	@CacheLookup
-	WebElement lname;
-	
-	@FindBy(how=How.ID, using="txtPassword")
-	@CacheLookup
+ 
+ @FindBy(how=How.NAME, using="pass")
+ @CacheLookup
 	WebElement pwd;
+ 
+ @FindBy(xpath="html/body/div/div[2]/form/table/tbody/tr[3]/td[2]/input")
+ @CacheLookup
+	WebElement login;
+ 
+ @FindBy(xpath="//*[@id='header']/h3[2]/a[1]")
+ @CacheLookup
+	WebElement add_new;
+ 
+ @FindBy(xpath="//*[@id='customer.customerName']")
+ @CacheLookup
+	WebElement cname;
+ 
+ 
+ @FindBy(xpath="//*[@id='customer.customerAddress']")
+ @CacheLookup
+	WebElement caddress;
+ 
+ @FindBy(xpath="//*[@id='customer.customerPancard']")
+ @CacheLookup
+	WebElement cpan;
+ 
+ @FindBy(xpath="//*[@id='customer.customerEmail']")
+ @CacheLookup
+	WebElement cemail;
+ 
+/* @FindBy(xpath="//*[@id='account.accountType']")
+ @CacheLookup
+	WebElement ctype;
+ public void setCtype() {
+		Select drptype=new Select(ctype);
+		drptype.selectByIndex(1);
+	}*/
 
-	@FindBy(how=How.ID, using="txtConfPassword")
-	@CacheLookup
-	WebElement cpwd;
+ 
+ 
+ 
+ @FindBy(xpath="//*[@id='account.accountBalance']")
+ @CacheLookup
+	WebElement cobal;
+ 
+ /*@FindBy(xpath="//*[@id='account.accountOpenDate']")
+ @CacheLookup
+	WebElement cdate;*/
+ 
+ @FindBy(xpath="//*[@id='user.userSecretQuestion']")
+ @CacheLookup
+	WebElement cqstn;
+ 
+ @FindBy(xpath="//*[@id='user.userTransPassword']")
+ @CacheLookup
+	WebElement cans;
+ 
+ @FindBy(xpath="//*[@id='admin']/table/tbody/tr[10]/td/input[1]")
+ @CacheLookup
+	WebElement cregister;
+ 
+ 
+    public LoginPageFactory(WebDriver driver) 
+    {
+	this.driver = driver;
+    }
 
-	public LoginPageFactory(WebDriver driver) {
-		this.driver = driver;
-	}
-	
-	public void typetext(String suname, String sfname, String slname, String spwd, String scpwd){
-		
-		uname.sendKeys(suname);
-		fname.sendKeys(sfname);
-		lname.sendKeys(slname);
-		pwd.sendKeys(spwd);
-		cpwd.sendKeys(scpwd);
-		fname.click();
-		driver.switchTo().alert().accept();
-		
-	}
-	
-	public void clear1(String spwd,String scpwd)
-	{
-		pwd.clear();
-		cpwd.clear();
-		pwd.sendKeys(spwd);
-		cpwd.sendKeys(scpwd);
-	}
-	
+
 	
 
+	public void typetext(String suname, String spwd, String scname, String scaddress, String scpan, String scemail, String scobal, String scans )
+    {
+    	admin_login.click();
+    	uname.sendKeys(suname);
+    	pwd.sendKeys(spwd);
+    	login.click();
+    	add_new.click();
+    	cname.sendKeys(scname);
+    	caddress.sendKeys(scaddress);
+    	cpan.sendKeys(scpan);
+    	cemail.sendKeys(scemail);
+    	Select drptype=new Select(driver.findElement(By.xpath("//*[@id='account.accountType']")));
+    	drptype.selectByVisibleText("Savings");
+    	cobal.sendKeys(scobal);
+    	//driver.findElements(By.xpath("//*[@id='user.userSecretQuestion']")).get(2).click();
+    	Select drptype2=new Select(driver.findElement(By.xpath("//*[@id='user.userSecretQuestion']")));
+    	drptype2.selectByVisibleText("What is your birth place?");
+    	cans.sendKeys(scans);
+    	cregister.click();
+    }
+	
+ 
 }
